64822479c67436d26466bfa8a49a9c04f296fc5d
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2015-10-21 14:40:57 +0000

    Created home page structure -static and app structure

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+
+ 
+$('.buttonAbout').click(function(e){
+     e.preventDefault();
+     $('.about-rules').show()
+});
+
+$('.buttonClose').click(function(e){
+    e.preventDefault();
+    $('.about-rules').hide()
+})
